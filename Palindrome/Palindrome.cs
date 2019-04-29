343f71b068fd008f308e62eef33af11f0e738cf0
Ben Sherman <48325127+bensherm@users.noreply.github.com>
2019-04-29 11:22:02 -0400

    Add files via upload

---
+++ Palindrome/Palindrome.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palindrome
+{
+   public class Palindrome
+    {
+        
+        public bool IsPalindrone(string userinput)
+        {
+            try
+            {
+                string msg = userinput;
+                char[] chArray = msg.ToCharArray();
+                int length = chArray.Length;
+
+                for (int i = 0; i < length / 2; i++)
+                {
+                    if (chArray[i] != chArray[length - i - 1])
+                        return false;
+                }
+
+                return true;
+            }
+
+            catch(Exception ex)
+            {
+                throw Exception;
+            }
+            finally
+            {
+                return false;
+            }
+         
+            }
+        }
+
+    }
+
---
+++ Palindrome/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palindrome
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            
+                Console.Write("Enter text to see if its a palindrome:");
+                string userinput = Console.ReadLine();
+                Palindrome Pal = new Palindrome();
+            try
+            {
+                Console.WriteLine(Pal.IsPalindrone(userinput));
+            }
+            catch
+            {
+                Console.WriteLine("There has been an Error");
+            }
+                do
+                {
+               
+                    Console.WriteLine("Try Again or Exit");
+                    userinput = Console.ReadLine();
+                if (userinput == "exit")
+                {
+                    break;
+                }
+
+                Console.WriteLine(Pal.IsPalindrone(userinput));
+                
+                } while (userinput != "exit");
+                
+            
+
+        }
+       
+    }
+}
